62e0fc3a2b3f4086af1089c8aaa579108279ec4d
anduolaya04 <36541393+anduolaya04@users.noreply.github.com>
2020-10-21 08:49:54 -0500

    All classes actualized

--- modules/src/main/java/com/platzi/functional/_07_inference/Inferencia.java
+++ modules/src/main/java/com/platzi/functional/_07_inference/Inferencia.java
@@ -1,5 +1,6 @@
 package com.platzi.functional._07_inference;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
@@ -7,6 +8,16 @@ public class Inferencia {
 
     public static void main(String[] args) {
         Function<Integer, String> funcionConvertidora = integer -> "Al doble:" + integer*2;
+
+        List<String> alumni = getList( "Andres", "Roland", "Erminio" );
+        alumni.forEach( (String name) -> System.out.println(name) );
+        alumni.forEach( (name) -> System.out.println(name) );
+        alumni.forEach( System.out::println);
     }
 
+
+    static <T> List<T> getList(T... elements) {
+
+        return Arrays.asList(elements);
+    }
 }
